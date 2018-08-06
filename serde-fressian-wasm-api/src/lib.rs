c1fe6d4119eafef68243a0f1d177c76c77ad40f0
Patrick Killean <pat.killean@gmail.com>
2018-08-06 15:11:02 -0400

    update wasm demo

--- serde-fressian-wasm-api/src/lib.rs
+++ serde-fressian-wasm-api/src/lib.rs
@@ -5,15 +5,14 @@ extern crate serde_fressian;
 
 // use std::mem;
 use serde::Serialize;
-use serde_fressian::ser::{to_vec, Serializer};
+use serde_fressian::ser::{Serializer};
 
 #[no_mangle]
 pub extern "C" fn hello() -> *const u8 {
-    let data = vec!["hello", "from", "wasm!"];
-    let buf = Vec::with_capacity(10);
+    let data = vec![["hello", "from", "wasm!"], ["isn't", "this", "exciting?!"]];
 
-    let mut FressianWriter = Serializer::new(buf);
-    data.serialize(&mut FressianWriter).unwrap();
-    FressianWriter.write_footer();
-    FressianWriter.get_ref().as_ptr()
+    let mut fressian_writer = Serializer::new();
+    data.serialize(&mut fressian_writer).unwrap();
+    fressian_writer.write_footer();
+    fressian_writer.get_ref().as_ptr()
 }
