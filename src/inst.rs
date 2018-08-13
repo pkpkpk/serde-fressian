40d4dea6bc299a375977e5795f4eb31fc6d905c1
Patrick Killean <pat.killean@gmail.com>
2018-08-13 12:24:48 -0400

    cleanup inst

--- src/inst.rs
+++ src/inst.rs
@@ -11,33 +11,31 @@ use imp::error::{Error};
 
 // from mentat/edn
 pub trait FromMillis {
-    fn from_millis(ts: i64) -> Self;
+    fn from_millis(ms: i64) -> Self;
 }
 
 impl FromMillis for DateTime<Utc> {
-    fn from_millis(ts: i64) -> Self {
-        Utc.timestamp(ts / 1_000, ((ts % 1_000).abs() as u32) * 1_000)
+    fn from_millis(ms: i64) -> Self {
+        Utc.timestamp(ms / 1_000, ((ms % 1_000).abs() as u32) * 1_000)
     }
 }
 
-
-// #[serde(remote = "DateTime")] // chrono::DateTime
-// pub struct DateTimeAlias<Tz: TimeZone> {
-//     datetime: NaiveDateTime,
-//     offset: Tz::Offset,
-// }
+impl FromMillis for Inst {
+    fn from_millis(ms: i64) -> Self {
+        Inst(DateTime::from_millis(ms))
+    }
+}
 
 #[derive(Shrinkwrap)]
 pub struct Inst (DateTime<Utc>);
-
 
 impl<'de> Deserialize<'de> for Inst {
     fn deserialize<D>(deserializer: D) -> Result<Inst, D::Error>
         where D: Deserializer<'de>,
     {
-        // let ms: i64 = deserializer.deserialize_any()?;
         let ms: i64 = i64::deserialize(deserializer)?;
-        let t = DateTime::from_millis(ms);
-        Ok(Inst(t))
+        // let t = DateTime::from_millis(ms);
+        Ok(Inst::from_millis(ms) )
     }
 }
+
