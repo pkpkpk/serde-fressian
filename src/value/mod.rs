22e4ddc9141749c3af4af261cc37d33f84ff3a46
Patrick Killean <pat.killean@gmail.com>
2018-09-06 22:01:49 -0400

    move serialize into mod

--- src/value/mod.rs
+++ src/value/mod.rs
@@ -21,7 +21,7 @@ use typed_arrays::*;
 use SET::{SET};
 
 mod de;
-mod ser;
+// mod ser;
 // mod from;
 // mod index;
 // mod partial_eq;
@@ -60,6 +60,7 @@ pub enum Value {
     // OBJECT_ARRAY
     // RECORD
     // TAGGED_OBJECT
+    // Rust types? Iter? Seq?
 }
 
 macro_rules! impl_into_value {
@@ -133,3 +134,39 @@ impl<'a> From<&'a[u8]> for Value {
     }
 }
 
+impl Serialize for Value {
+    #[inline]
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: ::serde::Serializer,
+    {
+        match *self {
+            Value::NULL => serializer.serialize_unit(),
+            Value::BOOL(b) => serializer.serialize_bool(b),
+            Value::INT(ref i) => i.serialize(serializer),
+            Value::FLOAT(ref f) => f.serialize(serializer),
+            Value::DOUBLE(ref d) => d.serialize(serializer),
+            Value::STRING(ref s) => serializer.serialize_str(s),
+            Value::LIST(ref v) => v.serialize(serializer), // newtype with cache call?!
+            Value::MAP(ref m) => m.serialize(serializer),
+            Value::SET(ref s) => s.serialize(serializer),
+            Value::KEY(ref v) => v.serialize(serializer),
+            Value::SYM(ref v) => v.serialize(serializer),
+            Value::INST(ref v) => v.serialize(serializer),
+            Value::REGEX(ref v) => v.serialize(serializer),
+            Value::URI(ref v) => v.serialize(serializer),
+            Value::UUID(ref v) => v.serialize(serializer),
+            Value::INT_ARRAY(ref v) => v.serialize(serializer),
+            Value::LONG_ARRAY(ref v) => v.serialize(serializer),
+            Value::FLOAT_ARRAY(ref v) => v.serialize(serializer),
+            Value::DOUBLE_ARRAY(ref v) => v.serialize(serializer),
+            Value::BOOLEAN_ARRAY(ref v) => v.serialize(serializer),
+            Value::BYTES(ref v) => serializer.serialize_bytes(v.as_ref()),
+
+            // CHAR(char)
+            // BIGINT()
+            // BIGDEC
+            // UTF8(&'a str),
+        }
+    }
+}
