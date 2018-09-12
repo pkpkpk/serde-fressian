2ecc9313cc56876d6ccf033c267c159e1b2c7354
Patrick Killean <pat.killean@gmail.com>
2018-09-11 21:41:11 -0400

    fress_fress

--- src/wasm.rs
+++ src/wasm.rs
@@ -4,13 +4,12 @@ use std::os::raw::{c_void};
 use de::{self};
 use ser::{self};
 use imp::io::{ByteWriter, IWriteBytes};
-use error::{Error, ErrorImpl};
+use error::{self};
 
 use serde::de::{Deserialize};
 use serde::ser::{Serialize, Serializer, SerializeMap};
 
-
-impl Serialize for Error {
+impl Serialize for error::Error {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
@@ -19,6 +18,7 @@ impl Serialize for Error {
     }
 }
 
+/// return a pointer to available memory of given byte length
 #[no_mangle]
 pub extern "C" fn fress_alloc(size: usize) -> *mut c_void {
     let mut buf = Vec::with_capacity(size);
@@ -27,14 +27,21 @@ pub extern "C" fn fress_alloc(size: usize) -> *mut c_void {
     return ptr as *mut c_void;
 }
 
+/// given a pointer to bytes originally requested by fress_alloc
+/// and written-to by javascript, reclaim them as a Vec<u8>
 pub fn ptr_to_vec(ptr: *mut u8, cap: usize) -> Vec<u8> {
     unsafe {
         std::slice::from_raw_parts(ptr, cap).to_vec()
     }
 }
 
-pub fn to_js<T: Serialize>(value: T) -> *mut c_void {
-    // let mut vec: Vec<u8> = ser::to_vec_footer(&value).unwrap();
+/// return a Vec<u8> back to rust and drop it
+#[no_mangle]
+pub extern "C" fn fress_free(ptr: *mut u8, cap: usize) {
+    let _ = ptr_to_vec(ptr, cap);
+}
+
+pub fn to_js<S: Serialize>(value: S) -> *mut c_void {
     let mut vec: Vec<u8> = ser::to_vec_footer(&value).unwrap_or_else(|err| ser::to_vec_footer(&err).unwrap());
     let ptr = vec.as_mut_ptr();
     mem::forget(vec);
