a536cf18c6ff388117438f51c6324cf2f42f9a00
Patrick Killean <pat.killean@gmail.com>
2018-09-23 02:24:39 -0400

    simplify vec to js, add from_ptr

--- src/wasm/mod.rs
+++ src/wasm/mod.rs
@@ -1,51 +1,63 @@
 use std::mem;
-use std::os::raw::{c_void};
 
-// use de::{self};
+use de::{self};
 use ser::{self};
+use error::{self};
 
-// use serde::de::{Deserialize};
+// use serde::de;
 use serde::ser::{Serialize};
 
-/// return a pointer to available memory of given byte length
-#[no_mangle]
-pub extern "C" fn fress_alloc(size: usize) -> *mut c_void {
-    let mut buf = Vec::with_capacity(size);
-    let ptr = buf.as_mut_ptr();
-    mem::forget(buf);
-    return ptr as *mut c_void;
-}
-
-/// given a pointer to bytes originally requested by fress_alloc
-/// and written-to by javascript, reclaim them as a Vec<u8>
-pub fn ptr_to_vec(ptr: *mut u8, cap: usize) -> Vec<u8> {
-    unsafe {
-        std::slice::from_raw_parts(ptr, cap).to_vec()
-    }
-}
-
-/// return a Vec<u8> back to rust and drop it
-#[no_mangle]
-pub extern "C" fn fress_free(ptr: *mut u8, cap: usize) {
-    let _ = ptr_to_vec(ptr, cap);
-}
-
+/// Transfer ownership of the bytes to javascript, and return a pointer to them.
+/// These bytes are unreachable to rust until they have been returned manually.
+/// see https://doc.rust-lang.org/std/mem/fn.forget.html
 #[inline]
-fn vec_to_ptr(mut vec: Vec<u8>) -> *mut c_void {
+fn vec_to_js(mut vec: Vec<u8>) -> *mut u8
+{
     let ptr = vec.as_mut_ptr();
+    // let len = vec.len();
     mem::forget(vec);
-    return ptr as *mut c_void;
+    ptr
 }
 
+/// for js consumers: request memory of the given byte length and return a pointer to it
+#[no_mangle]
+pub extern "C" fn fress_alloc(size: usize) -> *mut u8
+{
+    let buf = Vec::with_capacity(size);
+    vec_to_js(buf)
+}
 
-/// Serialize to a vec and give ownership to javascript via a pointer.
+/// for js consumers: return ownership of the bytes back to rust and drop them
+#[no_mangle]
+pub extern "C" fn fress_dealloc(ptr: *mut u8, len: usize)
+{
+    let _buf: Vec<u8> =  unsafe {
+        Vec::from_raw_parts(ptr, len, len)
+    };
+}
+
+/// Serialize to bytes and give ownership to javascript via a pointer.
 ///   - care should be taken by javascript consumers to read and free the bytes
 ///     synchronously and before any other calls/writes to the wasm module
 ///      - fress.wasm/read does this for you
 ///   - You can use Result<T,E> where T,E: Serialize; errors will be written
 ///     with an error code and picked up by the fress client as such
-pub fn to_js<S: Serialize>(value: S) -> *mut c_void {
+pub fn to_js<S: Serialize>(value: S) -> *mut u8
+{
     let vec: Vec<u8> = ser::to_vec(&value).unwrap_or_else(|err| ser::to_vec(&err).unwrap());
-    vec_to_ptr(vec)
+    vec_to_js(vec)
 }
 
+/// Given a pointer and length from javascript, deserialize fressian bytes to rust data structures.
+/// This does not take ownership of the bytes pointed to! The pointer & length should be kept so
+/// that the bytes can be returned and dropped according to the needs of your deserialized value.
+pub fn from_ptr<'a,T>(ptr: *mut u8, len: usize) -> error::Result<T>
+where
+    T: serde::de::Deserialize<'a>,
+{
+    let bytes: &[u8] =  unsafe {
+        std::slice::from_raw_parts(ptr, len)
+    };
+    let mut deserializer = de::Deserializer::from_bytes(bytes);
+    T::deserialize(&mut deserializer)
+}
